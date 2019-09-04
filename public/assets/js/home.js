be1233c53053ac9fe29a921e500592d2bb96a0f4
Sudhanshu Bhogal <sudhanshubhogal30299@gmail.com>
2019-09-04 20:03:34 +0530

    added homepage and twitter analysis page

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -1,36 +1,7 @@
-console.log('connected');
-var slider = document.getElementById('tweetsrange');
-var output = document.getElementById('numoftwtsinput');
-var searchtrend=document.getElementById("searchtrend");
-var toptweetstab=document.getElementById("nav-toptweets");
-output.innerHTML = slider.value;
-slider.addEventListener(
-	'input',
-	() => {
-		output.value = slider.value;
-	},
-	false
-);
-output.addEventListener('input', () => {
-	slider.value = output.value;
-});
-
-function runAnalyzer(){
-	fetch("/analyze?searchtrend="+searchtrend.value+"&ntwts="+slider.value)
-	.then(response=>response.json())
-	.then(jsondata=>{
-		toptweetstab.textContent=jsondata.text;
-		return;
-	});
-	return false;
-}
-
-document.onreadystatechange = function () {
-  var state = document.readyState
-  if (state == 'complete') {
-      setTimeout(function(){
-          document.getElementById('interactive');
-         document.getElementById('load').style.visibility="hidden";
-      },1500);
-  }
-}
+<script>
+    $(document).ready(function () {
+        setTimeout(function () {
+            $("body").addClass("loaded");
+        }, 3000);
+    });
+</script>
