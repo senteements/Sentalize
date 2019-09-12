281c9db9f387ea458344d579fa00051d72f58f68
Sudhanshu Bhogal <sudhanshubhogal30299@gmail.com>
2019-09-12 06:25:28 +0530

    added env vars and fixed some bugs

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 require("dotenv").config();
 var express = require("express"),
+  request = require("request"),
+  bodyParser = require("body-parser"),
   app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -13,22 +20,40 @@ app.get("/twitteranalysis", (req, res) => {
   res.render("twitter");
 });
 
-app.get("/analyze", (req, res) => {
-  var text;
-  console.log(req.query.searchtrend);
-  const { spawn } = require("child_process");
-  const pyProg = spawn("python", [
-    "public/assets/pyth.py",
-    req.query.searchtrend,
-    req.query.ntwts
-  ]);
+// for spawn process
+// app.get("/analyze", (req, res) => {
+//   var text;
+//   console.log(req.query.searchtrend);
+//   const { spawn } = require("child_process");
+//   const pyProg = spawn("python", [
+//     "public/assets/pyth.py",
+//     req.query.searchtrend,
+//     req.query.ntwts
+//   ]);
 
-  pyProg.stdout.on("data", function(data) {
-    text = data.toString();
-    console.log(text);
-    res.json({ text: text });
-  });
+//   pyProg.stdout.on("data", function(data) {
+//     text = data.toString();
+//     console.log(text);
+//     res.json({ text: text });
+//   });
+// });
+
+app.get("/analyze", (req, res) => {
+  var query = req.query.searchtrend;
+  var count = req.query.ntwts;
+
+  request(
+    process.env.FLASKSERVERURL + "/analyze?query=" + query + "&count=" + count,
+    function(err, response, body) {
+      if (!err && response.statusCode === 200) {
+        console.log("tweets : " + body); // Print the json response
+        tweetsjson = JSON.parse(body);
+        res.json({ text: tweetsjson });
+      }
+    }
+  );
 });
+
 app.listen(process.env.PORT, process.env.IP, () => {
   console.log("Server started!!");
 });
